710612fbf716a212caf5a9a2955309bc961dda3a
Andrew VanNess <avanness@student.bridgew.edu>
2021-09-23 22:04:38 -0400

    add base quiz component that queries wordpress api for data

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,16 +2,19 @@
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Quiz />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import Quiz from './components/Quiz.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    Quiz,
   },
 };
 </script>
---
+++ client/src/components/Quiz.vue
@@ -0,0 +1,38 @@
+<template>
+  <p>Quiz Here</p>
+</template>
+
+<script>
+export default {
+  name: 'Quiz',
+  created: function() {
+    this.fetchData();
+  },
+  data: function() {
+    return {
+      message: '',
+      userName: 'Andrew',
+      answerWeights: null,
+      questions: null,
+      characters: null,
+      currentQuestionIndex: 0,
+    };
+  },
+  methods: {
+    fetchData: function() {
+      const apiUrl = 'http://nexthink-quiz-interview.local/wp-json/api';
+      fetch(`${apiUrl}/get_questions`)
+        .then(resp => resp.json())
+        .then(json => (this.questions = json));
+
+          fetch(`${apiUrl}/get_characters`)
+        .then(resp => resp.json())
+        .then(json => (this.characters = json));
+
+    
+    },
+  },
+};
+</script>
+
+<style scoped></style>
